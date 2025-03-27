a1f5db04e1ffd7375bbb1d3fd963069e41bb493f
Anubhav Raj <anubhavr219@gmail.com>
2025-03-27 19:49:13 +0530

    some bugs

--- src/Components/GroceryOption.js
+++ src/Components/GroceryOption.js
@@ -4,9 +4,9 @@ import GroceryCard from "./GroceryCard";
 export default function GroceryOption()
 {
     return (
-        <div className="mt-20 w-[80%] container mx-auto">
+        <div className="w-[80%] container mx-auto mt-20">
             <h1>Shop Groceries on instamart</h1>
-            <div className="w-[80%] container mx-auto flex flex-wrap mt-20 gap-3">
+            <div className="container mx-auto flex flex-nowrap overflow-x-auto mt-5 gap-3">
                 {
                     GrocerGridCard.map((foodData)=><GroceryCard key={foodData.id} foodData={foodData}></GroceryCard>)
                 }
