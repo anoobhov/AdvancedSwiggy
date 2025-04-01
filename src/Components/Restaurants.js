eb0dd1fa5f7d1ca84a84024a2cd87b3a95deb17d
Anubhav Raj <anubhavr219@gmail.com>
2025-04-01 08:29:13 +0530

    Beautify and added infos in Restaurants list

--- src/Components/Restaurants.js
+++ src/Components/Restaurants.js
@@ -18,6 +18,9 @@ export default function Restaurant()
 
     if(RestData.length==0)
         return <Shimmer></Shimmer>
+
+
+    
     return(
         <div className="flex flex-wrap w-[80%] mx-auto mt-20 gap-5">
             {
